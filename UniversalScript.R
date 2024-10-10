d557e3ede0d01f0737e6229ce2ec40c219010974
Leandro Ledesma <leandroledesma237@yahoo.com>
2024-10-10 01:24:02 -0500

    Following Instructions Test

--- UniversalScript.R
+++ UniversalScript.R
@@ -7,6 +7,8 @@ library(readxl)
 # Set a pathway to where the data is saved
 SharePoint = "SharePoint1/"
 
+# Set working directory to SymbolicLink Test
+
 # Load in the data
 IDs <- read.csv(paste0(SharePoint,"PilotIDs.csv"))
 VisitLog <- read_excel(paste0(SharePoint, "ONRTestingVisitLog.xlsx"))
