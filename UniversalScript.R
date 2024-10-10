f1664eedba748083cf7b51f127f42ddc95f76bd7
Leandro Ledesma <leandroledesma237@yahoo.com>
2024-10-10 00:10:37 -0500

    Finalized Symbolic Link Example

---
+++ UniversalScript.R
@@ -0,0 +1,15 @@
+# This is a simple script to do some data manipulation
+
+# Load in packaged
+library(tidyverse)
+library(readxl)
+
+# Set a pathway to where the data is saved
+SharePoint = "SharePoint1/"
+
+# Load in the data
+IDs <- read.csv(paste0(SharePoint,"PilotIDs.csv"))
+VisitLog <- read_excel(paste0(SharePoint, "ONRTestingVisitLog.xlsx"))
+
+# Viwe the data
+view(IDs)
